2bdee04d3262ecd6643801294a8db9887c76db12
Hazel Baker-Harvey <84642569+hazelbakerharvey@users.noreply.github.com>
2021-09-29 20:07:15 -0700

    'readme'

--- transaction-api/src/main/java/com/ss/eastcoderbank/transactionapi/controller/TransactionController.java
+++ transaction-api/src/main/java/com/ss/eastcoderbank/transactionapi/controller/TransactionController.java
@@ -58,7 +58,6 @@ public class TransactionController {
         transactionService.postManyTransactions(transactions);
     }
 
-    @PermitAll
     @GetMapping("/health")
     @ResponseStatus(HttpStatus.OK)
     public void healthCheck() {}
