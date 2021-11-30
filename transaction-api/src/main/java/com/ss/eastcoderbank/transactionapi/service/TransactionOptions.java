674a831f509f3075addb12d09830f14910fecc67
asernass <alejandro.serna@smoothstack.com>
2021-11-29 23:24:51 -0500

    added logging

--- transaction-api/src/main/java/com/ss/eastcoderbank/transactionapi/service/TransactionOptions.java
+++ transaction-api/src/main/java/com/ss/eastcoderbank/transactionapi/service/TransactionOptions.java
@@ -2,10 +2,12 @@ package com.ss.eastcoderbank.transactionapi.service;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Getter
+@ToString
 @AllArgsConstructor
 public class TransactionOptions {
     private final String search;
